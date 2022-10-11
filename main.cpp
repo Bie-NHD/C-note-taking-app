7e7a32e223a7783d0c2f8881f970ebb0b814246e
tr1ssie <6251071106@st.utc2.edu.vn>
2022-10-11 21:03:49 +0700

    Thêm icon

--- main.cpp
+++ main.cpp
@@ -9,11 +9,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow w;
-
-    w.getTitle()->zoomIn(20);
-
-
-
+    QLabel *label;
+    w.getTitle()->zoomIn(16);
 
 
 
